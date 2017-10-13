6edbdb4b7c1551e3d12c434a6df2353e4488a435
Ata Gowani <atagowani@gmail.com>
2017-10-13 11:55:40 -0500

    Import CSS through Webpack

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import './src/style.css'
+
 import uirouter from '@uirouter/angularjs'
 var angular = require('angular')
 require('bootstrap')
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
+const webpack = require('webpack')
+
 module.exports = {
   entry: './app.js',
   output: {
     filename: 'vendor/bundle.js'
-  }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
+  },
 }
